0686def0800f9d02e5d0d5c2586a42817ce49022
ahmadou5 <78315869+ahmadou5@users.noreply.github.com>
2023-08-23 17:37:36 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -46,7 +46,7 @@ export default function Home() {
              <span className="w-3.5 h-3.5 bg-transparent absolute top-4 -right-[18px] rounded-tl-[11px] shadow-myShadow2"></span>
           </span>
           {Menus.map((menu, i) => (
-              <li key={i} className="w-16">
+              <li key={i} className="w-20">
                   <div className=" flex flex-col text-center pt-6 " onClick={() => {setActive(i)}}>
                      <span className={`text-xl cursor-pointer duration-500 ${i === active && '-mt-6 text-white'}`}><ion-icon name={menu.icon}></ion-icon></span>
                      <span className={` ${active === i ? 'translate-y-4 duration-700 opacity-100' : 'opacity-0 translate-y-10'}`}>{menu.name}</span>
