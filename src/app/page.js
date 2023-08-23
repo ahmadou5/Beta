2955a02df8c2179b5d8cd2cdbb594719eda49dc4
ahmadou5 <ahmadlasauwal@gmail.com>
2023-08-23 19:17:45 +0300

    'initialisatio==54'

--- src/app/page.js
+++ src/app/page.js
@@ -47,7 +47,6 @@ export default function Home() {
           </span>
           {Menus.map((menu, i) => (
               <li key={i} className="w-16">
-                  c
                   <div className=" flex flex-col text-center pt-6 " onClick={() => {setActive(i)}}>
                      <span className={`text-xl cursor-pointer duration-500 ${i === active && '-mt-6 text-white'}`}><ion-icon name={menu.icon}></ion-icon></span>
                      <span className={` ${active === i ? 'translate-y-4 duration-700 opacity-100' : 'opacity-0 translate-y-10'}`}>{menu.name}</span>
