7e1bf4ff9cf0e66ef688a3b1cc17d5585a6aa428
ahmadou5 <ahmadlasauwal@gmail.com>
2023-08-24 13:35:29 +0300

    'initialisation==100'

--- src/app/page.js
+++ src/app/page.js
@@ -7,10 +7,10 @@ export default function Home() {
   const [active, setActive] = useState(0)
    const Menus = [
       {name: 'Home', icon: 'home' , dis: 'translate-x-0', ref: ''},
-      {name: 'Swap', icon: 'swap' , dis: 'translate-x-16', ref: 'swap'},
-      {name: 'Earn', icon: 'leaf' , dis: 'translate-x-32', ref: 'earn'},
-      {name: 'Mint', icon: 'water' , dis: 'translate-x-48', ref: 'mint'},
-      {name: 'Wallet', icon: 'wallet' , dis: 'translate-x-64', ref: 'wallet'},
+      {name: 'Swap', icon: 'swap' , dis: 'translate-x-15', ref: 'swap'},
+      {name: 'Earn', icon: 'leaf' , dis: 'translate-x-31', ref: 'earn'},
+      {name: 'Mint', icon: 'water' , dis: 'translate-x-45', ref: 'mint'},
+      {name: 'Wallet', icon: 'wallet' , dis: 'translate-x-65', ref: 'wallet'},
   ]
 
   const view = () => {
@@ -37,19 +37,19 @@ export default function Home() {
       <h1>hello</h1>
       {view ()}
     </div>
-    <div className=" mb-0 ml-0 mr-0 bg-transparent fixed bottom-0">
+    <div className=" mb-0 ml-0 mr-0 bg-transparent fixed bottom-0 w-full">
     <div className="bg-white max-h-[4.4rem] w-[95%] px-6 ml-auto mr-auto rounded-t-xl lg:hidden md:hidden">
       <ul className="flex relative">
           
-          <span className={`bg-blue-500 border-4 duration-500 ${Menus[active].dis} border-black h-16 w-16 absolute rounded-full -top-5`}>
+          {/**<span className={`bg-blue-500 border-4 duration-500 ${Menus[active].dis} border-black h-16 w-16 absolute rounded-full -top-6`}>
              <span className="w-3.5 h-3.5 bg-transparent absolute top-4 -left-[18px] rounded-tr-[11px] shadow-myShadow1" ></span>
              <span className="w-3.5 h-3.5 bg-transparent absolute top-4 -right-[18px] rounded-tl-[11px] shadow-myShadow2"></span>
-          </span>
+  </span>**/}
           {Menus.map((menu, i) => (
               <li key={i} className="w-16">
                   <div className=" flex flex-col text-center pt-6 " onClick={() => {setActive(i)}}>
                      <span className={`text-xl cursor-pointer duration-500 ${i === active && '-mt-6 text-white'}`}><ion-icon name={menu.icon}></ion-icon></span>
-                     <span className={` ${active === i ? 'translate-y-4 duration-700 opacity-100' : 'opacity-0 translate-y-10'}`}>{menu.name}</span>
+                     <span className={` ${active === i ? 'translate-y-0 duration-700 text-blue-600 opacity-100' : 'opacity-0 translate-y-14'}`}>{menu.name}</span>
                   </div> 
               </li>
           ))}
