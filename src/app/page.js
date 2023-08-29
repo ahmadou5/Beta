3dc6a077da60a460f46913ed0e372956feb6ba64
ahmadou5 <ahmadlasauwal@gmail.com>
2023-08-29 22:42:18 +0300

    UI

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 'use client'
 import { Nav } from "@/components/Nav";
+
 import { useState } from "react";
 
 
@@ -15,8 +16,14 @@ export default function Home() {
   const hello = () => {
     if( active === 0) {
       return(<>
-      <div className="ml-6 mr-6 mt-5 bg-inherit text-start text-ellipsis text-4xl">
-        <h1 className="text-white font-thin font-sans">Hello</h1>
+      <div className="ml-6 mr-6 mt-20 mb-5 bg-inherit items-center text-start text-ellipsis text-4xl">
+        <h1 className="text-white font-thin font-sans">👋🏽 Welcome to</h1>
+        
+        <h1 className="text-[#359cfc] font-thin font-sans">YieldZone</h1>
+
+       {/**  <div className=" ml-auto mr-3 text-sm items-end">
+          <button className="w-40 h-10 py-1 px-1 rounded-3xl bg-[#359cfc] drop-shadow-3xl shadow-3xl hover:bg-[#589ee0] hover:rounded-2xl"> Connect Wallet </button>
+      </div>**/}
       </div>
       </>
       )
@@ -26,8 +33,21 @@ export default function Home() {
     if( active === 0) {
       return(
       <div>
-          <div className="w-[90%] ml-auto mr-auto mt-2 bg-slate-600 max-h-[5rem]">
-            <p>ggg</p>
+          <div className="w-[100%] h-auto  mt-2 ">
+            <div className="py-4 px-4 ">
+              <div className="w-full bg-[#0B0E11] rounded-full py-0 px-0">
+                <img className='ml-auto mr-auto w-[95%] h-[50%]' src='./trade.png' />
+              </div>
+              <div className="mt-5 bg-[#0B0E11] flex items-center w-[99%] h-[10rem] py-1 px-1  rounded-md">
+                <p className="py-1 px-1 font-thin">Experience the future of earning with YieldZone Stake securely, earn effortlessly, and shape the decentralized landscape. Unleash your assets' potential today for rewards tomorrow</p>
+              </div>
+            </div>
+          </div>
+          <div className=" ml-auto mr-auto text-sm flex items-center">
+            <button className="w-40 h-10 py-1 px-1 rounded-3xl ml-auto mr-auto mb-auto bg-[#359cfc] hover:bg-[#589ee0] hover:rounded-2xl"> Explore </button>
+          </div>
+          <div className="bg-[#0B0E11] max-h-[6rem] h-6 w-[100%] px-6 ml-auto mt-20 mr-auto rounded-xl lg:hidden md:hidden">
+            <div className="h-[5rem]">gg</div>
           </div>
       </div>
       )
@@ -55,7 +75,7 @@ export default function Home() {
       {view ()}
     </div>
     <div className=" mb-0 ml-0 mr-0 bg-transparent fixed bottom-0 w-full">
-    <div className="bg-slate-700 max-h-[4rem] w-[95%] px-6 ml-auto mr-auto rounded-t-xl lg:hidden md:hidden">
+    <div className="bg-[#0B0E11] max-h-[4rem] w-[95%] px-6 ml-auto mr-auto rounded-t-xl lg:hidden md:hidden">
       <ul className="flex relative">
           
           {/**<span className={`bg-blue-500 border-4 duration-500 ${Menus[active].dis} border-black h-16 w-16 absolute rounded-full -top-6`}>
@@ -65,8 +85,8 @@ export default function Home() {
           {Menus.map((menu, i) => (
               <li key={i} className="w-20">
                   <div className=" flex flex-col text-center pt-6 " onClick={() => {setActive(i)}}>
-                     <span className={`text-xl cursor-pointer duration-500 ${i === active && '-mt-6 text-slate-700'}`}><ion-icon name={menu.icon}></ion-icon></span>
-                     <span className={` -mt-1 ${active === i ? 'translate-y-0 duration-700 text-blue-400 opacity-100' : 'opacity-0 translate-y-15'}`}>{menu.name}</span>
+                     <span className={`text-xl cursor-pointer duration-500 ${i === active && '-mt-6 text-[#0B0E11]'}`}><ion-icon name={menu.icon}></ion-icon></span>
+                     <span className={` -mt-1 ${active === i ? 'translate-y-0 duration-700 text-[#359cfc] opacity-100' : 'opacity-0 translate-y-15'}`}>{menu.name}</span>
                   </div> 
               </li>
           ))}
