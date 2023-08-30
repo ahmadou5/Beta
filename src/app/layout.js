1db6e7fed889b151662828e58066dd91874f24f2
ahmadou5 <ahmadlasauwal@gmail.com>
2023-08-30 16:44:09 +0300

    dev-ahmadou-3-poppins-added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter, Poppins } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
-
+//const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({subsets: ['devanagari'],weight:'500'})
 
 export const metadata = {
   title: 'YieldZone',
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
-      <body className={`${inter.className} bg-[#cbced1] text-slate-900  `}>{children}</body>
+      <body className={`${poppins.className} bg-[#cbced1] text-slate-300  `}>{children}</body>
     </html>
   )
 }
